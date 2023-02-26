5d8bfda620adae76ad33322b7668582857f2b2fb
Corentinrhr <62744229+Corentinrhr@users.noreply.github.com>
2023-02-26 17:07:07 +0100

    Génération aléatoire des mdp

---
+++ PasswordManager/CS_BackEnd/PasswordGenerator.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace PasswordManager
+{
+    public partial class MainWindow : Window
+    {
+        private void Click_Generate(object sender, RoutedEventArgs e) 
+        {
+            int length = int.Parse(TextBoxLongueur.Text);
+            bool useLowercase = Checkbox_Minuscule.IsChecked == true;
+            bool useUppercase = Checkbox_Majuscule.IsChecked == true;
+            bool useNumbers = Checkbox_Nombre.IsChecked == true;
+            bool useSymbols = Checkbox_Symbole.IsChecked == true;
+            if (useLowercase == false && useUppercase == false && useNumbers == false && useSymbols == false)
+            {
+                MessageBox.Show("Veuillez cocher au moins une case pour générer votre mot de passe");
+            }
+            else
+            {
+                Add_MDP.Text = GeneratePassword(length, useLowercase, useUppercase, useNumbers, useSymbols);
+            }
+        }
+        private static string GeneratePassword(int length, bool useLowercase, bool useUppercase, bool useNumbers, bool useSymbols)
+        {
+            const string lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
+            const string uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            const string numberChars = "0123456789";
+            const string symbolChars = "!@#$%^&*()_+-=[]{};':./<>?";
+            StringBuilder passwordBuilder = new StringBuilder(length);
+            Random random = new Random();
+
+            // Ajouter des caractères de chaque type si la CheckBox correspondante est cochée
+            if (useLowercase)
+            {
+                passwordBuilder.Append(lowercaseChars[random.Next(lowercaseChars.Length)]);
+            }
+            if (useUppercase)
+            {
+                passwordBuilder.Append(uppercaseChars[random.Next(uppercaseChars.Length)]);
+            }
+            if (useNumbers)
+            {
+                passwordBuilder.Append(numberChars[random.Next(numberChars.Length)]);
+            }
+            if (useSymbols)
+            {
+                passwordBuilder.Append(symbolChars[random.Next(symbolChars.Length)]);
+            }
+
+            // Ajouter des caractères aléatoires jusqu'à atteindre la longueur souhaitée
+            while (passwordBuilder.Length < length)
+            {
+                string availableChars = "";
+                if (useLowercase)
+                {
+                    availableChars += lowercaseChars;
+                }
+                if (useUppercase)
+                {
+                    availableChars += uppercaseChars;
+                }
+                if (useNumbers)
+                {
+                    availableChars += numberChars;
+                }
+                if (useSymbols)
+                {
+                    availableChars += symbolChars;
+                }
+                passwordBuilder.Append(availableChars[random.Next(availableChars.Length)]);
+            }
+
+            return passwordBuilder.ToString();
+            
+        }
+    }
+}
