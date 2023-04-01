dfd08d3fda0e0cb3a4bcc022489f45bc085e9707
Corentin <corahier@hotmail.fr>
2023-04-01 21:47:23 +0200

    Bug fix : bouton ouverture lorsque URL incorrect

--- PasswordManager/CS_FrontEnd/ClickEvent.cs
+++ PasswordManager/CS_FrontEnd/ClickEvent.cs
@@ -85,7 +85,11 @@ namespace PasswordManager
 
         private void Click_open_website(object sender, EventArgs e)
         {
-            Process.Start(new ProcessStartInfo() { FileName = URLWebSiteItem.Text.ToString(), UseShellExecute = true });
+            try
+            {
+                Process.Start(new ProcessStartInfo() { FileName = URLWebSiteItem.Text.ToString(), UseShellExecute = true });
+            }
+            catch { }
         }
 
         private void Click_copy_mdp(object sender, EventArgs e)
